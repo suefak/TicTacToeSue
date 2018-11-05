f11825cd814a9e1f3edb3d30f8631674e6af4174
suefak <suefakunle@gmail.com>
2018-11-05 09:21:40 -0500

    changed linked in config file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production:https: 'https://aqueous-atoll-85096.herokuapp.com',
   development: 'https://tic-tac-toe-wdi.herokuapp.com/'
 }
 
